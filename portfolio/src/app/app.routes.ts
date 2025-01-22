dc840c0851303d09bc402b0d4f301f699340036c
Jan Hibeljic <j.hibeljic@gmx.de>
2025-01-22 15:40:49 +0100

    my structure and routes linked

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { SectionsComponent } from './sections/sections.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: SectionsComponent }];
--- portfolio/src/app/sections/sections.component.ts
+++ portfolio/src/app/sections/sections.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from './hero/hero.component';
+import { MenubarComponent } from './menubar/menubar.component';
+import { WhymeComponent } from './whyme/whyme.component';
+import { MyskillsComponent } from './myskills/myskills.component';
+import { MyprojectsComponent } from './myprojects/myprojects.component';
+import { ReferencesComponent } from './references/references.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { BackgroundComponent } from './background/background.component';
 
 @Component({
   selector: 'app-sections',
   standalone: true,
-  imports: [],
+  imports: [
+    HeroComponent,
+    MenubarComponent,
+    WhymeComponent,
+    MyskillsComponent,
+    MyprojectsComponent,
+    ReferencesComponent,
+    ContactComponent,
+    FooterComponent,
+    BackgroundComponent,
+  ],
   templateUrl: './sections.component.html',
-  styleUrl: './sections.component.scss'
+  styleUrl: './sections.component.scss',
 })
-export class SectionsComponent {
-
-}
+export class SectionsComponent {}
