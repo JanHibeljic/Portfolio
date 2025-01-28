7ad01f17960c55f49c57fd05c74c48d6e79194bb
Jan Hibeljic <j.hibeljic@gmx.de>
2025-01-28 21:42:56 +0100

    profile picture and background first try

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SectionsComponent } from './sections/sections.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SectionsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio';
--- portfolio/src/app/sections/hero/hero.component.ts
+++ portfolio/src/app/sections/hero/hero.component.ts
@@ -5,8 +5,16 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
+  public isHovering = false;
 
+  onMouseOver(): void {
+    this.isHovering = true;
+  }
+
+  onMouseLeave(): void {
+    this.isHovering = false;
+  }
 }
